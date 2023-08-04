fd689025f1b9b3001d5727582e5df6a085ad49d2
Yasmeen Shaik <bellaryshaikyasmeen@gmail.com>
2023-08-04 20:48:00 +0530

    Added Code for ZoomIn

--- app/src/main/java/com/example/locationapik/MainActivity.kt
+++ app/src/main/java/com/example/locationapik/MainActivity.kt
@@ -4,17 +4,42 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.PopupMenu
+import android.widget.Toast
 import com.google.android.gms.common.api.GoogleApi
+import com.google.android.gms.common.api.Status
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.AutocompleteFragment
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private var mGoogleMap:GoogleMap?=null
-
+private lateinit var autoccompleteFragment:AutocompleteSupportFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Places.initialize(this,getString(R.string.apiKey))
+        autoccompleteFragment=supportFragmentManager.findFragmentById(R.id.auto_complete_fragemnet)
+        as AutocompleteSupportFragment
+        autoccompleteFragment.setPlaceFields(listOf(Place.Field.ID,Place.Field.ADDRESS,Place.Field.LAT_LNG))
+        autoccompleteFragment.setOnPlaceSelectedListener(object :PlaceSelectionListener{
+            override fun onError(p0: Status) {
+             Toast.makeText(this@MainActivity,"Some Error in Search",Toast.LENGTH_LONG).show()
+            }
+
+            override fun onPlaceSelected(p0: Place) {
+
+                val latling=p0.latLng!!
+                zoomIn(latling)
+            }
+
+        })
         val mapFragment = supportFragmentManager.findFragmentById(
             R.id.maps_fragment
         ) as? SupportMapFragment
@@ -29,6 +54,11 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         mapbtn.setOnClickListener {
             popmenu.show()
         }
+    }
+    fun zoomIn(latling:LatLng)
+    {
+        val newLatLing=CameraUpdateFactory.newLatLngZoom(latling,12f)
+        mGoogleMap?.animateCamera(newLatLing)
     }
 
     private fun changeMenuitem(itemid: Int) {
