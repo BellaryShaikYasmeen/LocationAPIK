d055c24e6e6652b07df03db7352e3791663edcf3
Yasmeen Shaik <bellaryshaikyasmeen@gmail.com>
2023-08-04 16:50:06 +0530

    diffrenttypesof maps

--- app/src/main/java/com/example/locationapik/MainActivity.kt
+++ app/src/main/java/com/example/locationapik/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.locationapik
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.PopupMenu
 import com.google.android.gms.common.api.GoogleApi
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -17,6 +19,27 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             R.id.maps_fragment
         ) as? SupportMapFragment
         mapFragment?.getMapAsync (this)
+      val mapbtn:ImageButton=findViewById(R.id.mapOptionsMenu)
+        val popmenu=PopupMenu(this,mapbtn)
+        popmenu.menuInflater.inflate(R.menu.map_option,popmenu.menu)
+        popmenu.setOnMenuItemClickListener {menuItem->
+            changeMenuitem(menuItem.itemId)
+            true
+        }
+        mapbtn.setOnClickListener {
+            popmenu.show()
+        }
+    }
+
+    private fun changeMenuitem(itemid: Int) {
+        when(itemid)
+        {
+            R.id.normal->mGoogleMap?.mapType=GoogleMap.MAP_TYPE_NORMAL
+            R.id.hybrid->mGoogleMap?.mapType=GoogleMap.MAP_TYPE_HYBRID
+            R.id.satellite->mGoogleMap?.mapType=GoogleMap.MAP_TYPE_SATELLITE
+            R.id.terrain->mGoogleMap?.mapType=GoogleMap.MAP_TYPE_TERRAIN
+
+        }
 
     }
 
